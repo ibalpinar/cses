3407ea9167b03847fa03a5475d5b89cc2f10057a
ibalpinar <ilker@balpinar.com>
2024-04-29 23:45:16 +0300

    Add the solution of increasing array

---
+++ 01-introductory-problems/04-increasing-array/app.js
@@ -0,0 +1,15 @@
+function getIterationCount(N, array){
+   var iterationCount = 0;
+   for(var i = 1; i < N; i++){
+       if(array[i] < array[i-1]){
+           iterationCount += array[i-1] - array[i];
+           array[i] = array[i-1];
+       }
+   }
+   return iterationCount;
+}
+
+const N =8;
+const array = [8, 2, 4, 2, 6, 2, 8, 2];
+const iterationCount = getIterationCount(N, array);
+console.log(iterationCount);
