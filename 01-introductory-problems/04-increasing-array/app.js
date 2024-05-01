afa6c59f4c43efddb2d67229a970e6b59dc360cc
ibalpinar <ilker@balpinar.com>
2024-05-01 04:22:11 +0300

    Fix format of code

--- 01-introductory-problems/04-increasing-array/app.js
+++ 01-introductory-problems/04-increasing-array/app.js
@@ -9,7 +9,7 @@ function getIterationCount(N, array){
    return iterationCount;
 }
 
-const N =8;
+const N = 8;
 const array = [8, 2, 4, 2, 6, 2, 8, 2];
 const iterationCount = getIterationCount(N, array);
 console.log(iterationCount);
